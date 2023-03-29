12ce3772a836ea7567b297a4420fcc474ff365f6
numq <numq.dev@gmail.com>
2023-03-29 15:07:24 +0300

    Provide refactoring

--- src/main/kotlin/coroutine/CloseableCoroutineScope.kt
+++ src/main/kotlin/coroutine/CloseableCoroutineScope.kt
@@ -4,8 +4,9 @@ import kotlinx.coroutines.*
 import java.io.Closeable
 import kotlin.coroutines.CoroutineContext
 
-internal class CloseableCoroutineScope
-constructor(dispatcher: CoroutineDispatcher = Dispatchers.Default) : CoroutineScope, Closeable {
+internal class CloseableCoroutineScope(
+    dispatcher: CoroutineDispatcher = Dispatchers.Default,
+) : CoroutineScope, Closeable {
     override val coroutineContext: CoroutineContext = dispatcher + SupervisorJob()
     override fun close() = coroutineContext.cancel()
 }
